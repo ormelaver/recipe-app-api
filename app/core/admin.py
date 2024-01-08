648407458049ec3d6d2439d2f11fa6772c906f60
Or Melaver <ormelaver@gmail.com>
2024-01-08 17:12:58 +0200

    committed for checking - drf

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,46 @@
-from django.contrib import admin # noqa
+"""
+Django admin customization
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _ #good practice. Not needed for us as we don't really translate
 
-# Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users"""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',), #css classes you can add (makes the page looks a little nicer)
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
+
+admin.site.register(models.User, UserAdmin)
