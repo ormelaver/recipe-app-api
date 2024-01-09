e23638aa320424bb3e1e676b813d7a9cfc58eeca
Or Melaver <ormelaver@gmail.com>
2024-01-09 16:29:33 +0200

    adding token creation

--- app/user/views.py
+++ app/user/views.py
@@ -7,7 +7,7 @@ from rest_framework.settings import api_settings
 
 from user.serializers import (
     UserSerializer,
-    AuthTokenSerializer
+    AuthTokenSerializer,
 )
 
 
@@ -16,7 +16,7 @@ class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
 
 
-class CreatetokenView(ObtainAuthToken):
+class CreateTokenView(ObtainAuthToken):
     """Create a new auth token for user"""
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
