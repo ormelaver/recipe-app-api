6780e9e17e30d9e326c65704960c7b36bb57bc16
Or Melaver <ormelaver@gmail.com>
2024-01-09 17:25:20 +0200

    code check

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -22,6 +22,15 @@ class UserSerializer(serializers.ModelSerializer):
         """Create and return a user with encrypted password"""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """Update and return user"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
 
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for the user auth token"""
@@ -46,4 +55,3 @@ class AuthTokenSerializer(serializers.Serializer):
 
         attrs['user'] = user
         return attrs
-
