3212330d39d7c28f5000444c26fb480b47816a5b
Or Melaver <ormelaver@gmail.com>
2024-01-23 16:02:13 +0200

    added tags and ingredients

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -77,28 +77,28 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(res.data[0]['name'], tag.name)
         self.assertEqual(res.data[0]['id'], tag.id)
 
-    # def test_update_tag(self):
-    #     """Test updating a tag."""
-    #     tag = Tag.objects.create(user=self.user, name='After Dinner')
+    def test_update_tag(self):
+        """Test updating a tag."""
+        tag = Tag.objects.create(user=self.user, name='After Dinner')
 
-    #     payload = {'name': 'Dessert'}
-    #     url = detail_url(tag.id)
-    #     res = self.client.patch(url, payload)
+        payload = {'name': 'Dessert'}
+        url = detail_url(tag.id)
+        res = self.client.patch(url, payload)
 
-    #     self.assertEqual(res.status_code, status.HTTP_200_OK)
-    #     tag.refresh_from_db()
-    #     self.assertEqual(tag.name, payload['name'])
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        tag.refresh_from_db()
+        self.assertEqual(tag.name, payload['name'])
 
-    # def test_delete_tag(self):
-    #     """Test deleting a tag."""
-    #     tag = Tag.objects.create(user=self.user, name='Breakfast')
+    def test_delete_tag(self):
+        """Test deleting a tag."""
+        tag = Tag.objects.create(user=self.user, name='Breakfast')
 
-    #     url = detail_url(tag.id)
-    #     res = self.client.delete(url)
+        url = detail_url(tag.id)
+        res = self.client.delete(url)
 
-    #     self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
-    #     tags = Tag.objects.filter(user=self.user)
-    #     self.assertFalse(tags.exists())
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        tags = Tag.objects.filter(user=self.user)
+        self.assertFalse(tags.exists())
 
     # def test_filter_tags_assigned_to_recipes(self):
     #     """Test listing tags to those assigned to recipes."""
